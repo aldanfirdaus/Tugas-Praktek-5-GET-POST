08f6389458626aa997c4878d509269b0ce9abc0d
aldanfirdaus <79752660+aldanfirdaus@users.noreply.github.com>
2021-03-24 23:07:45 +0700

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,8 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="2; url=http://localhost/coba/latihan%205/praktikum1_1.php"> 
+    </head>
+    <body>
+        Tes Redirect
+    </body>
+</html>

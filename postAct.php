58327d6e38e405e525f6fa7a5373f5117a03ba25
aldanfirdaus <79752660+aldanfirdaus@users.noreply.github.com>
2021-03-24 23:03:30 +0700

    Add files via upload

---
+++ postAct.php
@@ -0,0 +1,6 @@
+<?php
+//mencetak nama dengan menangkap nama nilai melalui variabel $_POST
+echo "<center>Nama :".$_POST['nama']."</center><br>";  
+//mencetak email dengan menangkap nama nilai melalui variabel $_POST
+echo "<center>Email :".$_POST['email']."</center><br>"; 
+?>
